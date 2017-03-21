a509fde666b8589b40f5d63bc081ac68b1f44078
Ephraim Schnaidman <ephraim.schnaidman@my.liu.edu>
2017-03-21 15:28:26 -0400

    Adding sentiment words part to termProject

--- Term Project/termProject.R
+++ Term Project/termProject.R
@@ -119,6 +119,91 @@ wordcloud(head(dm$word, 50), head(dm$freq, 50), random.order=FALSE, colors=brewe
 
 # 4 - Sentiment Analysis (Ephraim)
 
+#********************************************
+#         Sentiment Analysis
+#********************************************
+#R's c() function (for "combine") allows us to add a few industry- and Twitter-specific terms to form our final pos.words and neg.words vectors:
+
+pos.words = scan('positive-words.txt', what='character', comment.char=';')
+neg.words = scan('negative-words.txt', what='character', comment.char=';')
+
+neg.words = c(neg.words, 'wtf', 'fail')
+
+#Implementing our sentiment scoring algorithm
+require(plyr)
+require(stringr)
+require(stringi)
+
+score.sentiment = function(sentences, pos.words, neg.words, .progress='none')
+{
+  
+  # we got a vector of sentences. plyr will handle a list
+  # or a vector as an "l" for us
+  # we want a simple array of scores back, so we use
+  # "l" + "a" + "ply" = "laply":
+  scores = laply(sentences, function(sentence, pos.words, neg.words) {
+    
+    # clean up sentences with R's regex-driven global substitute, gsub():
+    sentence = gsub('[[:punct:]]', '', sentence)
+    sentence = gsub('[[:cntrl:]]', '', sentence)
+    sentence = gsub('\\d+', '', sentence)
+    # and convert to lower case:
+    #sentence = tolower(sentence)
+    
+    # split into words. str_split is in the stringr package
+    word.list = str_split(sentence, '\\s+')
+    # sometimes a list() is one level of hierarchy too much
+    words = unlist(word.list)
+    
+    # compare our words to the dictionaries of positive & negative terms
+    pos.matches = match(words, pos.words)
+    neg.matches = match(words, neg.words)
+    
+    # match() returns the position of the matched term or NA
+    # we just want a TRUE/FALSE:
+    pos.matches = !is.na(pos.matches)
+    neg.matches = !is.na(neg.matches)
+    
+    # and conveniently enough, TRUE/FALSE will be treated as 1/0 by sum():
+    score = sum(pos.matches) - sum(neg.matches)
+    
+    return(score)
+  }, pos.words, neg.words, .progress=.progress )
+  
+  scores.df = data.frame(score=scores, text=sentences)
+  return(scores.df)
+}
+
+sentiment.scores= score.sentiment(tweets, pos.words, neg.words, .progress='text')
+
+score= sentiment.scores$score
+hist(score)
+score = subset(score,score!=0)
+sentiscore = data.frame(score)
+
+#topic.scores= score.topic(tweets, sports.words, .progress='text')
+#topic.mentioned = subset(topic.scores, score !=0)
+
+#N= nrow(topic.scores)
+#Nmentioned = nrow(topic.mentioned)
+
+#dftemp=data.frame(topic=c("Mentioned", "Not Mentioned"), 
+#                 number=c(Nmentioned,N-Nmentioned))
+
+Pos= nrow(sentiment.scores)
+Neg= nrow(sentiscore)
+Ntrl= nrow(NULL)
+
+dftemp=data.frame(words=c("Positive", "Negative", "Neutral"),
+                  number=c(Neg,Pos-Neg,!Pos||Neg))
+
+library("googleVis")
+Pie<- gvisPieChart(dftemp, options=list(
+  legend="{ position: 'top', maxLines:2 }",
+  colors="['#5C3292', '#1A8763', '#871B47']",
+  width=400, height=360))
+plot(Pie)
+
 
 
 # 5 - User Profile (Monica & MD)
