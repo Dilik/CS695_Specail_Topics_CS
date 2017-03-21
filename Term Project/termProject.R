6c816fcd941dac30f6b029d13bfa6805ab3a8c6b
Dilmurod <dilmurod.makhamadaliev@my.liu.edu>
2017-03-21 14:57:30 -0400

    Necessary packages for TermProject Added

--- Term Project/termProject.R
+++ Term Project/termProject.R
@@ -4,7 +4,43 @@
 
 # 1 - Load data (MD)
 
+# Install necessary packages
 
+install.packages('tm')
+install.packages('RColorBrewer')
+install.packages('wordcloud')
+install.packages("readr")
+install.packages("tm")
+install.packages("RColorBrewer")
+install.packages("wordcloud")
+install.packages("plyr")
+install.packages("stringr")
+install.packages("googleVis")
+install.packages("stringi")
+install.packages("magrittr")
+install.packages("dplyr")
+install.packages("mclust")
+install.packages("igraph")
+install.packages("sna")
+source("http://bioconductor.org/biocLite.R")
+biocLite("RBGL")
+biocLite("graph")
+
+
+library('tm')
+library('RColorBrewer')
+library('wordcloud')
+library("googleVis")
+library("plyr")
+library(readr)
+library(mclust)
+library(sna)
+library(graph)
+library(igraph)
+library("stringr")
+library("stringi")
+library("magrittr")
+library("dplyr")
 
 # 2 - Word Cloud (MD)
 
